efd5e4aa9c08ebc443a60c5b9c4522be552470c9
Douglas <douglasas@edu.univali.br>
2019-06-20 02:59:17 -0300

    Theta simplified

--- hdl/theta/theta_pkg.vhd
+++ hdl/theta/theta_pkg.vhd
@@ -1,15 +1,16 @@
 library ieee;
 use ieee.std_logic_1164.all;
+
 library work;
+use work.main_pkg.all;
 use work.slogic_pkg.all;
+use work.atan2_pkg.all;
 
 package theta_pkg is
 	----------size--------------------
-	constant IMAGE_X				: integer := 10;
-	constant IMAGE_Y				: integer := 10;
-	constant SIZE_IMAGE			: integer := IMAGE_X * IMAGE_Y;
-	constant REGS_QT				: integer := 22;
-	constant SIZE_REGIS_COMP	: integer := SIZE_IMAGE;
+	constant IMAGE_X         : integer := INPUT_IMAGE_X-4;
+	constant IMAGE_Y         : integer := INPUT_IMAGE_Y-4;
+	constant QT_CYCLES_ATAN2 : integer := ANGLES_TABLE_SIZE+2;
 
   component theta_top is
   	port(
@@ -26,37 +27,5 @@ package theta_pkg is
   		theta_o		:	out	slogic
 		);
 	end component theta_top;
-
-  component register1
-  	generic(
-  		TAMNHO_REGS : integer
-  	);
-  	port(
-  ------------imput----------------
-      set_i		:	in integer;
-  	 valid_i		:	in std_logic;
-  ------------sync-----------------
-      rstn_i		:	in	std_logic;
-      clk_i 		:	in	std_logic;
-  -----------output----------------
-      regi_out	: 	out integer
-  );
-  end component register1;
-
-  component register_desl
-  	generic(
-  		TAMNHO_REGS : integer
-  	);
-  	port(
-  ------------imput----------------
-      set_i		:	in std_logic;
-  	 valid_i		:	in std_logic;
-  ------------sync-----------------
-      rstn_i		:	in	std_logic;
-      clk_i 		:	in	std_logic;
-  -----------output----------------
-      regis_out	: 	out std_logic
-  );
-  END component register_desl;
 
 end theta_pkg;

635a66a34964e4ca7a15a9fe9d75f547bcfe25e0
Douglas <douglasas@edu.univali.br>
2019-06-28 15:32:30 -0300

    Gradient testbench adapted

--- sim/gradient/gradient_tb.vhd
+++ sim/gradient/gradient_tb.vhd
@@ -1,5 +1,7 @@
 library ieee;
+use ieee.numeric_std.all;
 use ieee.std_logic_1164.all;
+use ieee.math_real.all;
 
 library work;
 use work.slogic_pkg.all;
@@ -9,17 +11,20 @@ entity gradiente_tb is
 end entity;
 
 architecture arch of gradiente_tb is
-  constant period : time := 10 ns;
+  constant period : time := 10 ps;
   signal rstn : std_logic := '0';
   signal clk  : std_logic := '1';
 
   signal valid : std_logic := '0';
-  signal pix_x : slogic := to_slogic(0);
-  signal pix_y : slogic := to_slogic(0);
+  signal pix_x : slogic;
+  signal pix_y : slogic;
 
   signal valid_o  : std_logic;
   signal pix_o : slogic;
 
+  signal real_pix_x : real := 0.0;
+  signal real_pix_y : real := 25.0;
+  signal real_pix_o : real;
 begin
 
   rstn <= '1' after period/2;
@@ -30,18 +35,22 @@ begin
     wait for period/2;
 
     valid <= '1';
-    pix_x <= x"00000000";
-    pix_y <= x"00000000";
+    real_pix_x <= 0.0;
+    real_pix_y <= 24.0;
 
     for i in 0 to 24 loop
-      pix_x <= pix_x + to_slogic(1);
-      pix_y <= pix_y + to_slogic(1);
+      real_pix_x <= real_pix_x + 1.0;
+      real_pix_y <= real_pix_y - 1.0;
 
       wait for period;
     end loop;
     valid <= '0';
     wait;
   end process;
+
+  pix_x <= to_slogic(real_pix_x);
+  pix_y <= to_slogic(real_pix_y);
+  real_pix_o <= real(to_integer(signed(pix_o))) / (2.0**LSB);
 
   gradient_inst : gradient_top PORT MAP (
     ------------input------------

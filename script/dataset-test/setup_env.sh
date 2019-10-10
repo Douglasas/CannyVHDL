e42c552caac361ad2f5bb0c175b8ebdc6dca5dd8
Douglas Santos <dalmeidados@caocim.lirmm.fr>
2019-10-10 15:59:29 +0200

    LIRMM server Modelsim

--- script/dataset-test/setup_env.sh
+++ script/dataset-test/setup_env.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
+#export PATH=$PATH:/opt/intel/19.2/modelsim_ase/bin
+
+# LIRMM Modelsim License
 export LM_LICENSE_FILE=5280@mentor.cnfm.fr
-export PATH=$PATH:/opt/intel/19.2/modelsim_ase/bin
+# LIRMM Modelsim
+export MODELSIM_HOME=/soft/Mentor/ModelSim_SE_10.3/modeltech/
+export PATH=$MODELSIM_HOME/bin:$PATH
